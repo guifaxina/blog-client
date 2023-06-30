2ea19ff44bf25d83c118a9364a1cdd8226c09845
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-30 09:08:45 -0300

    chore: add components with placeholders to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,94 +1,104 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import Link from "next/link";
-import { BlogPost } from "@/components/BlogPost";
-import { MainBlogPost } from "@/components/MainBlogPost";
-import { LastReadsPost } from "@/components/LastPostsPost";
+import { Post, Header, LastReads, MainPost } from "@/components/blog";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import "swiper/css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
   return (
     <main className="bg-zinc-50 h-screen flex flex-col">
       <header className="bg-zinc-50 w-full border border-b-1 p-2 flex justify-around">
-        <div>Logo</div>
-
-        <ul className="flex flex-row justify-center gap-6 ">
-          <li>
-            <Link
-              href=""
-              className="hover:text-zinc-500 transition-colors duration-300"
-            >
-              Business
-            </Link>
-          </li>
-          <li>
-            <Link
-              href=""
-              className="hover:text-zinc-500 transition-colors duration-300"
-            >
-              Culture
-            </Link>
-          </li>
-          <li>
-            <Link
-              href=""
-              className="hover:text-zinc-500 transition-colors duration-300"
-            >
-              Science
-            </Link>
-          </li>
-          <li>
-            <Link
-              href=""
-              className="hover:text-zinc-500 transition-colors duration-300"
-            >
-              Technology
-            </Link>
-          </li>
-        </ul>
-
-        <div className="flex gap-4">
-          <button>Login</button>
-          <button>SignUp</button>
-        </div>
+        <Header />
       </header>
 
       <div className="flex gap-16 justify-center">
         <section className="flex justify-center">
           <div className="border-zinc-900 border-t-4 mt-10 max-w-3xl justify-center">
             <h2 className="bg-zinc-900 text-white p-1.5 inline">Trending</h2>
-            <MainBlogPost />
+            <MainPost 
+            thumbnail="/images/jeff-bezos.jpeg"
+            altText="Jeff Bezos"
+            tag={"TECHNOLOGY"}
+            estimatedReadTime={11}
+            title={"Jeff Bezos Doesn't Stop Getting Balder it is Unbelievable"}
+            author={"Guilherme Faxina"}
+            />
           </div>
         </section>
 
         <aside className="flex justify-center">
           <div className="border-t-4 border-zinc-900 mt-10 max-w-xs">
             <h2 className="bg-zinc-900 text-white p-1.5 inline">Last Posts</h2>
-            <LastReadsPost />
-            <LastReadsPost />
-            <LastReadsPost />
-            <LastReadsPost />
-            <LastReadsPost />
+            <LastReads />
+            <LastReads />
+            <LastReads />
+            <LastReads />
+            <LastReads />
           </div>
         </aside>
       </div>
 
       <section className="flex justify-center mt-6">
         <div className="border-t-4 border-zinc-800  max-w-6xl">
-          <h2 className="bg-zinc-900 inline p-1.5 text-white">Weekly's Most Reads</h2>
+          <h2 className="bg-zinc-900 inline p-1.5 text-white">Weeklys Most Reads</h2>
           <div className="flex flex-row justify-center gap-6">
             <Swiper
               spaceBetween={20}
               slidesPerView={4}
             >
-              <SwiperSlide> <BlogPost /> </SwiperSlide>
-              <SwiperSlide> <BlogPost /> </SwiperSlide>
-              <SwiperSlide> <BlogPost /> </SwiperSlide>
-              <SwiperSlide> <BlogPost /> </SwiperSlide>
-              <SwiperSlide> <BlogPost /> </SwiperSlide>
+              <SwiperSlide> 
+                  <Post 
+                  author="Guilherme Faxina"
+                  thumbnail="/images/jeff-bezos.jpeg"
+                  altText="Jeff"
+                  estimatedReadTime={1}
+                  tag={"BUSINESS"}
+                  title={"Jeff Bezos is Getting Balder"}
+                  /> 
+              </SwiperSlide>
+              <SwiperSlide> 
+                  <Post 
+                  author="Guilherme Faxina"
+                  thumbnail="/images/jeff-bezos.jpeg"
+                  altText="Jeff"
+                  estimatedReadTime={1}
+                  tag={"BUSINESS"}
+                  title={"Jeff Bezos is Getting Balder"}
+                  /> 
+              </SwiperSlide>
+              <SwiperSlide> 
+                  <Post 
+                  author="Guilherme Faxina"
+                  thumbnail="/images/jeff-bezos.jpeg"
+                  altText="Jeff"
+                  estimatedReadTime={1}
+                  tag={"BUSINESS"}
+                  title={"Jeff Bezos is Getting Balder"}
+                  /> 
+              </SwiperSlide>
+              <SwiperSlide> 
+                  <Post 
+                  author="Guilherme Faxina"
+                  thumbnail="/images/jeff-bezos.jpeg"
+                  altText="Jeff"
+                  estimatedReadTime={1}
+                  tag={"BUSINESS"}
+                  title={"Jeff Bezos is Getting Balder"}
+                  /> 
+              </SwiperSlide>
+              <SwiperSlide> 
+                  <Post 
+                  author="Guilherme Faxina"
+                  thumbnail="/images/jeff-bezos.jpeg"
+                  altText="Jeff"
+                  estimatedReadTime={1}
+                  tag={"BUSINESS"}
+                  title={"Jeff Bezos is Getting Balder"}
+                  /> 
+              </SwiperSlide>
             </Swiper>
           </div>
         </div>
