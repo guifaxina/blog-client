081e21f0a2ab68cbde6c95f4de98a85cd324f127
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-06-30 09:05:42 -0300

    chore: create header component

---
+++ src/components/blog/Header.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <>
+      <div>Logo</div>
+
+      <ul className="flex flex-row justify-center gap-6 ">
+        <li>
+          <Link
+            href="/blog/business"
+            className="hover:text-zinc-500 transition-colors duration-300"
+          >
+            Business
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/blog/culture"
+            className="hover:text-zinc-500 transition-colors duration-300"
+          >
+            Culture
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/blog/science"
+            className="hover:text-zinc-500 transition-colors duration-300"
+          >
+            Science
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/blog/technology"
+            className="hover:text-zinc-500 transition-colors duration-300"
+          >
+            Technology
+          </Link>
+        </li>
+      </ul>
+
+      <div className="flex gap-4">
+        <button>Login</button>
+        <button>SignUp</button>
+      </div>
+    </>
+  );
+}
