c4e05481b5b3374cac67f52a8d525f080e820fd5
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-04 13:04:20 -0300

    chore: create useCreateAuthor hook

---
+++ src/hooks/useCreateAuthor.ts
@@ -0,0 +1,14 @@
+import { useMutation, gql } from "@apollo/client";
+
+export default function useCreateAuthor() {
+  const CREATE_AUTHOR = gql`
+    mutation CreateAuthor($type: CreateAuthorInput!) {
+      createAuthor(input: $type) {
+        id
+      }
+    }
+  `;
+  const [createAuthor, { data, loading, error }] = useMutation(CREATE_AUTHOR);
+
+  return { createAuthor, data, loading, error }
+}
