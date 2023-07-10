2171a5813c94dbf8f3adf882e0e0cf9fab1bfc8b
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-07-10 08:40:18 -0300

    chore: create index with exports

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,4 +1,4 @@
-export { MainBlogPost as MainPost } from "./MainBlogPost"
-export { Header } from "./Header"
-export { LastReadsPost as LastReads } from "./LastReadsPost"
-export { BlogPost as Post } from "./BlogPost"
+export { MainBlogPost as MainPost } from "./MainBlogPost";
+export { Header } from "./Header";
+export { LastReadsPost as LastReads } from "./LastReadsPost";
+export { BlogPost as Post } from "./BlogPost";
