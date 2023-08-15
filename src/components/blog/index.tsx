09686c98fd59860d8dffa569b5964ff60dc07ff9
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-08-15 07:59:39 -0300

    chore: add profiledropdown component export

--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -2,3 +2,4 @@ export { MainBlogPost as MainPost } from "./MainBlogPost";
 export { Header } from "./Header";
 export { LastReadsPost as LastReads } from "./LastReadsPost";
 export { BlogPost as Post } from "./BlogPost";
+export { ProfileDropdown } from "./ProfileDropdown"
